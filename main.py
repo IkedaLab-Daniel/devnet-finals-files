a11e06cf1b2ed7dcf6ac753b83fd5d68eb49b21e
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-25 18:22:57 +0800

    Print improvement

--- main.py
+++ main.py
@@ -16,21 +16,22 @@ player = {
 
 def playerDetail():
     print(f"""
-    |---------- Player Status ------------|
-            ATK     -       {player["ATK"]}
-            DEF     -       {player['DEF']}
-            GOLD    -       {player["GOLD"]}g
-    |-------------------------------------| 
+        |---------- Player Status ------------|
+                ATK     -       {player["ATK"]}
+                DEF     -       {player['DEF']}
+                GOLD    -       {player["GOLD"]}g
+        |-------------------------------------| 
  """)
     
 def playerBag():
-    print("     ------------- Bag ---------------")
+    print("        |------------- Bag ---------------|")
     if len(player["BAG"]) != 0:
         for i, item in enumerate(player["BAG"], start=1):
-            print(f"{i} | {item}")
+            print(f"                {i} | {item[0]} +{item[2]} {item[1]}")
+        print("        |---------------------------------|")
     else:
         print("              No bag item yet")
-        print("     ----------------------------------")
+        print("         ----------------------------------")
 def menu():
     print(""" 
         |--------------- Menu --------------|
@@ -48,32 +49,35 @@ def buy():
         print(f"{item}: {details}")
     print("============================")
     buy_item = input("Enter item NAME to buy: ")
-    stat_name = shop[buy_item][0]
-    stat_value = shop[buy_item][1]
-    price = (shop[buy_item][2])
-    # print(stat_name)
-    # print(stat_value)
-    # print(price)
-    if (int(player["GOLD"]) > price):
-         new_player_gold = player["GOLD"] - price
-         player["GOLD"] = new_player_gold
-    else:
-         print("Insufficient Gold")
+    try:
+        stat_name = shop[buy_item][0]
+        stat_value = shop[buy_item][1]
+        price = (shop[buy_item][2])
+        # print(stat_name)
+        # print(stat_value)
+        # print(price)
+        if (int(player["GOLD"]) > price):
+            new_player_gold = player["GOLD"] - price
+            player["GOLD"] = new_player_gold
+        else:
+            print("Insufficient Gold")
 
-    if stat_name == "ATK":
-        player_atk = int(player["ATK"]) + stat_value
-        player["ATK"] = player_atk
-    elif stat_name == "DEF":
-        player_def = int(player["DEF"]) + stat_value
-        player["DEF"] = player_def
-    else:
-        print("not ATK not DEF")
+        if stat_name == "ATK":
+            player_atk = int(player["ATK"]) + stat_value
+            player["ATK"] = player_atk
+        elif stat_name == "DEF":
+            player_def = int(player["DEF"]) + stat_value
+            player["DEF"] = player_def
+        else:
+            print("not ATK not DEF")
 
-    # ? Add to bag
-    player["BAG"].append(buy_item)
-    print("======= SUCCESS: NEW PLAYER DETAIL ===========")
-    playerDetail()
-    print("==============================================")
+        # ? Add to bag
+        player["BAG"].append([buy_item, stat_name, stat_value])
+        print("\n\n======= SUCCESS: NEW PLAYER DETAIL ===========")
+        playerDetail()
+        print("==============================================")
+    except:
+        print("\n! >>>>>>>> Item does not exist <<<<<<<<<<<< !")
 
 
 
