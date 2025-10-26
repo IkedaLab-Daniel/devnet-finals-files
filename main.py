025608cbaf3b0a4e84065412a420d488a207ba45
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-26 12:20:00 +0800

    MVP

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
+import json
 
 def load_data():
-    pass
+    with open(USER_DATA, 'r') as file:
+        data = json.load(file)
+        return data
 
-def save_data():
-    pass
+# > append data 
+def save_data(username ,data_to_add):
+    with open(f"{username}.json", 'x') as file:
+        pass
+
+    with open(f"{username}.json", 'w') as file:
+        json.dump(data_to_add, file)
+
 
 def register():
-    pass
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    bio = input("Enter bio: ")
+
+    to_add = {
+        f"{username}": {
+            "password": f"{password}",
+            "bio": f"{bio}",
+            "posts": []
+        }
+    }   
+    print(to_add)
+    save_data(username ,to_add)
 
 def login():
-    pass
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    data = load_data()
+    if username in data and data[username] == password:
+        print("Login successful!")
+    else:
+        print("Invalid username or password.")
 
 def view_profile():
     pass
@@ -22,3 +49,27 @@ def create_post():
 
 def view_user_post():
     pass
+
+def main():
+    while True:
+        print("""
+        |----- Welcome to FakeBook -----|
+        |      1      |   Login         |
+        |      2      |   Register      |
+        |--------------------------------
+        |        'quit' to exit         |
+        |-------------------------------|
+        """)
+        
+        choice = input('Choose: ')
+        if choice == '1':
+            login()
+        elif choice == '2':
+            register()
+        elif choice == 'quit':
+            break
+        else:
+            print("Invalid input")
+
+
+main()
