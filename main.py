e8268ba4fe80e39c9c02d8aadcced0cdeb7b1024
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-26 12:20:00 +0800

    start

---
+++ main.py
@@ -0,0 +1,24 @@
+
+def load_data():
+    pass
+
+def save_data():
+    pass
+
+def register():
+    pass
+
+def login():
+    pass
+
+def view_profile():
+    pass
+
+def update_bio():
+    pass
+
+def create_post():
+    pass
+
+def view_user_post():
+    pass
